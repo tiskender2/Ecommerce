7aef151fac2ef1b2bf9ecbc16ebc1963ab92e2f9
tiskender2 <tolgaiskendr@gmail.com>
2017-10-26 09:44:49 +0300

    Initial Commit

---
+++ Eticaret/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eticaret
+//
+//  Created by MacMini on 26.10.2017.
+//  Copyright © 2017 MacMini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ EticaretUITests/EticaretUITests.swift
@@ -0,0 +1,36 @@
+//
+//  EticaretUITests.swift
+//  EticaretUITests
+//
+//  Created by MacMini on 26.10.2017.
+//  Copyright © 2017 MacMini. All rights reserved.
+//
+
+import XCTest
+
+class EticaretUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
